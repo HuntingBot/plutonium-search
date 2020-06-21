d176e90b7e50bf335bfbd7b7931d53cede5b92a5
HuntingBot <41249395+HuntingBot@users.noreply.github.com>
2020-06-21 22:18:15 +0800

    Version 2.0
    Finished!

--- plutoniumsearch.py
+++ plutoniumsearch.py
@@ -1,18 +1,50 @@
 import golly as g
 
 # Parameters
-bx = 4
-by = 4
-boring_periods = [1, 2, 6]
+w = int(g.getstring("Search width:", "8"))
+h = int(g.getstring("Search height:", "8"))
+sym = g.getstring("Symmetry (C1, D2)", "D2")
+seed = int(g.getstring("Slice number:"))
+boring_periods = [1, 2, 3, 4, 6, 8, 10]
+preset = int(g.getstring("Preset cells:", "8"))
 
 # Do not change this
 patt_count = 0
 cnt = [1] * 999
 
+def convert(seed):
+	global bx
+	known_cells = ""
+	res = ""
+	while seed >= 2:
+		res += str(seed % 2)
+		seed /= 2
+	if seed:
+		res += str(seed)
+	res += "0" * (preset - len(res))
+	res = res[::-1]
+	for i in range(len(res)):
+		if i % bx == 0:
+			known_cells += "$"
+		if res[i] == "0":
+			known_cells += "b"
+		else:
+			known_cells += "o"
+	return known_cells
+
+def symmetrify(known_cells, sym):
+	global h
+	rows = known_cells.split("$")
+	if h % 2 == 0:
+		rows.extend(rows[::-1])
+	else:
+		rows.extend(rows[-1::-1])
+	return "$".join(rows)
+
 def dfs(known_cells, depth, cell_count):
 	global bx, by
 	if depth >= bx * by:
-		run_patt(known_cells)
+		run_patt(symmetrify(known_cells, sym))
 		return
 	if depth % bx == 0:
 		dfs(known_cells + "$b", depth + 1, cell_count)
@@ -65,5 +97,17 @@ def run_patt(known_cells):
 	g.show(str(patt_count) + "/" + str(2 ** (bx * by)))
 
 g.autoupdate(True)
-dfs("", 0, 0)
+if sym == "C1":
+	bx = w
+	by = h
+elif sym == "D2":
+	bx = w
+	if h % 2 == 0:
+		by = h / 2
+	else:
+		by = h / 2 + 1
+else:
+	g.note("Not a valid symmetry!")
+	g.exit()
+dfs(convert(seed), preset, preset)
 #run_patt("$3o$2o$o!")
